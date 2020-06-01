0504c79c251ccf36cbb95d0f57d09a02fb443c0a
a1kageyama <36861752+a1kageyama@users.noreply.github.com>
2020-06-02 06:31:59 +0900

    Update StreamDropWhile.java
    Java9's Stream sample

--- StreamDropWhile.java
+++ StreamDropWhile.java
@@ -5,8 +5,8 @@ public class StreamDropWhile {
 	public static void main(String[] args) {
 
 		Stream<Integer> numStream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-											  11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
-											  1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+							11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+							1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 		// Exclude the matching values from top of the list
 		numStream.dropWhile(b -> {
